7e5cfe97311b8ea6097dacf0086f355f01c94109
Dion Dokter <dion@tweedegolf.com>
2023-03-24 13:52:44 +0100

    Added test and implemented car driving

--- simulator/src/lib.rs
+++ simulator/src/lib.rs
@@ -18,6 +18,125 @@ impl Simulator {
     }
 
     pub fn tick(&mut self, delta_time: f32) {
-        
+        self.current_road_users
+            .retain_mut(|user| user.tick(&self.road_network, delta_time));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::road::Node;
+    use nalgebra::Point3;
+    use std::f32::consts::PI;
+
+    #[test]
+    fn single_node_one_car() {
+        let mut simulator = Simulator::new(RoadNetwork::new(
+            [
+                (
+                    0,
+                    Node::new(
+                        0,
+                        Point3::new(8.0, 0.0, 0.0),
+                        30.0 / 3.6, // 30kph
+                        vec![1],
+                        None,
+                        None,
+                    ),
+                ),
+                (
+                    1,
+                    Node::new(
+                        1,
+                        Point3::new(10.0, 0.0, 0.0),
+                        10.0 / 3.6, // 30kph
+                        vec![2, 4],
+                        None,
+                        None,
+                    ),
+                ),
+                (
+                    2,
+                    Node::new(
+                        2,
+                        Point3::new(10.5, 1.0, 0.0),
+                        5.0 / 3.6, // 30kph
+                        vec![3],
+                        None,
+                        None,
+                    ),
+                ),
+                (
+                    3,
+                    Node::new(
+                        3,
+                        Point3::new(11.0, 20.0, 0.0),
+                        30.0 / 3.6, // 30kph
+                        Vec::new(),
+                        None,
+                        None,
+                    ),
+                ),
+                (
+                    4,
+                    Node::new(
+                        4,
+                        Point3::new(10.5, -1.0, 0.0),
+                        5.0 / 3.6, // 30kph
+                        vec![5],
+                        None,
+                        None,
+                    ),
+                ),
+                (
+                    5,
+                    Node::new(
+                        5,
+                        Point3::new(11.0, -20.0, 0.0),
+                        30.0 / 3.6, // 30kph
+                        Vec::new(),
+                        None,
+                        None,
+                    ),
+                ),
+            ]
+            .into(),
+        ));
+
+        simulator.current_road_users.push(RoadUser::new(
+            Point3::new(0.0, 0.0, 0.0),
+            0.0,
+            3.5,
+            5.0,
+            PI / 2.0,
+            0,
+            5,
+            &simulator.road_network,
+        ));
+
+        let mut current_time = 0.0;
+        const DELTA_TIME: f32 = 0.01;
+        for i in 0..2000 {
+            if i % 100 == 0 {
+                println!("Current time: {current_time}");
+
+                for user in simulator.current_road_users.iter() {
+                    println!(
+                        "  @{}, => {:?} @ {} m/s",
+                        user.location(),
+                        user.current_direction(),
+                        user.current_speed()
+                    );
+                }
+            }
+
+            simulator.tick(DELTA_TIME);
+            current_time += DELTA_TIME;
+
+            if simulator.current_road_users.is_empty() {
+                break;
+            }
+        }
     }
 }
--- simulator/src/user.rs
+++ simulator/src/user.rs
@@ -1,14 +1,16 @@
-use nalgebra::{Point3, Vector3, Quaternion, UnitQuaternion};
+use nalgebra::{Point3, Rotation2, Vector3};
+use ordered_float::OrderedFloat;
 
 use crate::road::RoadNetwork;
 
+#[derive(Debug)]
 pub struct RoadUser {
     location: Point3<f32>,
     current_direction: Vector3<f32>,
     current_speed: f32,
 
-    acceleration: f32, // m/s/s
-    deceleration: f32, // m/s/s
+    acceleration: f32,       // m/s/s
+    deceleration: f32,       // m/s/s
     max_steering_angle: f32, // rads/s
 
     next_node: u32,
@@ -16,22 +18,114 @@ pub struct RoadUser {
 }
 
 impl RoadUser {
-    pub fn tick(&mut self, network: &RoadNetwork, delta_time: f32) {
+    pub fn new(
+        location: Point3<f32>,
+        current_speed: f32,
+        acceleration: f32,
+        deceleration: f32,
+        max_steering_angle: f32,
+        next_node: u32,
+        destination_node: u32,
+        network: &RoadNetwork,
+    ) -> Self {
+        Self {
+            location,
+            current_direction: (network.find_node(next_node).location() - location).normalize(),
+            current_speed,
+            acceleration,
+            deceleration,
+            max_steering_angle,
+            next_node,
+            destination_node,
+        }
+    }
+
+    pub fn tick(&mut self, network: &RoadNetwork, delta_time: f32) -> bool {
         let next_node = network.find_node(self.next_node);
 
-        let target_speed = next_node.max_speed;
+        let target_speed = next_node.max_speed();
 
         let speed_difference = target_speed - self.current_speed;
         if self.current_speed < target_speed {
-            self.current_speed += (self.acceleration * delta_time).max(speed_difference);
+            self.current_speed += (self.acceleration * delta_time).min(speed_difference);
         } else if self.current_speed > target_speed {
-            self.current_speed -= (self.deceleration * delta_time).min(speed_difference);
+            self.current_speed -= (self.deceleration * delta_time).max(speed_difference);
         }
 
-        let target_direction = (self.location - next_node.location).normalize();
-        let angle_difference = self.current_direction.xy().angle(&target_direction.xy());
-        let steering_angle = angle_difference.max(self.max_steering_angle); // TODO: Something with steering angle acceleration
+        let target_direction = (self.location - next_node.location()).normalize();
 
-        // UnitQuaternion::rotation_between(target_direction., b)
+        let total_rotation =
+            Rotation2::rotation_between(&target_direction.xy(), &self.current_direction.xy());
+
+        let total_rotation_angle = total_rotation.angle();
+        let max_steering_angle = self.max_steering_angle * delta_time;
+
+        self.current_direction = nalgebra::Rotation3::new(Vector3::new(
+            0.0,
+            0.0,
+            total_rotation_angle
+                .min(max_steering_angle)
+                .max(-max_steering_angle),
+        ))
+        .transform_vector(&self.current_direction);
+        self.current_direction.z = target_direction.z;
+        self.current_direction = self.current_direction.normalize();
+
+        self.location += self.current_direction * self.current_speed * delta_time;
+
+        if (self.location - next_node.location()).magnitude() < 0.5 {
+            if self.next_node == self.destination_node {
+                println!("Reached destination");
+                return false;
+            }
+
+            print!("{} @ {} => ", next_node.id, next_node.location());
+
+            let Some(next_node) = self.find_next_path_node(network) else {
+                println!("Could not find a path");
+                return false;
+            };
+
+            self.next_node = next_node;
+        }
+
+        true
+    }
+
+    fn find_next_path_node(&self, network: &RoadNetwork) -> Option<u32> {
+        let current_node = network.find_node(self.next_node);
+        let destination_node = network.find_node(self.destination_node);
+
+        let (next_path, _) = pathfinding::directed::astar::astar(
+            &current_node,
+            |test_node| {
+                test_node
+                    .next_nodes(network)
+                    .map(|next_node| (next_node, OrderedFloat(current_node.distance_to(next_node))))
+            },
+            |test_node| {
+                OrderedFloat(test_node.distance_to(destination_node))
+            },
+            |test_node| {
+                *test_node == destination_node
+            }
+        )?;
+
+        let next_path_node = next_path.get(1)?;
+        println!("{} @ {}", next_path_node.id, next_path_node.location());
+
+        Some(next_path_node.id)
+    }
+
+    pub fn location(&self) -> Point3<f32> {
+        self.location
+    }
+
+    pub fn current_direction(&self) -> Vector3<f32> {
+        self.current_direction
+    }
+
+    pub fn current_speed(&self) -> f32 {
+        self.current_speed
     }
 }
